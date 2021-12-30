d27730e61824eb21c8e90f4b1ea866de5ab8c92d
chme <christian.meffert@googlemail.com>
2021-12-30 13:06:33 +0100

    wip! [vue3] Readd vue-progressbar
    Switch to `@aacassandra/vue3-progressbar` for vue3 support.
    Looks like the original version is not supported any more:
    https://github.com/hilongjw/vue-progressbar/issues/98

--- web-src/src/main.js
+++ web-src/src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import store from './store'
+import VueProgressBar from '@aacassandra/vue3-progressbar'
 import App from './App.vue'
 
 createApp(App)
   .use(store)
+  .use(VueProgressBar, {
+    color: 'hsl(204, 86%, 53%)',
+    failedColor: 'red',
+    height: '1px'
+  })
   .mount('#app')
