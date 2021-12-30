d9617e1d180331fb674d1294ec7488dc7575f512
chme <christian.meffert@googlemail.com>
2021-12-30 13:13:57 +0100

    wip! [vue3] Migrate to vue-router 4.x

--- web-src/src/main.js
+++ web-src/src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import store from './store'
+import { router } from './router'
 import VueProgressBar from '@aacassandra/vue3-progressbar'
 import App from './App.vue'
 
 createApp(App)
   .use(store)
+  .use(router)
   .use(VueProgressBar, {
     color: 'hsl(204, 86%, 53%)',
     failedColor: 'red',
