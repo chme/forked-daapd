6471bdc1992138f71dfce86aba077119f0d9704a
chme <christian.meffert@googlemail.com>
2021-12-31 13:57:45 +0100

    wip! [vue3] Readd vue-observe-visibility
    Instructions taken from <https://github.com/Akryum/vue-observe-visibility/issues/219>

--- web-src/src/main.js
+++ web-src/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import store from './store'
 import { router } from './router'
 import VueProgressBar from '@aacassandra/vue3-progressbar'
+import { ObserveVisibility } from 'vue-observe-visibility'
 import App from './App.vue'
 
 createApp(App)
@@ -11,5 +12,10 @@ createApp(App)
     color: 'hsl(204, 86%, 53%)',
     failedColor: 'red',
     height: '1px'
+  })
+  .directive('observe-visibility', {
+    beforeMount: ObserveVisibility.beforeMount,
+    update: ObserveVisibility.update,
+    unmounted: ObserveVisibility.unbind
   })
   .mount('#app')
