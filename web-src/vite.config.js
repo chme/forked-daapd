a30ae0bed47d45bbc2185deb64f37fb623d7dbcc
chme <christian.meffert@googlemail.com>
2021-12-31 15:04:09 +0100

    wip! [vue3] Add dev server proxy to vite.config.js
    Proxying based on .env.development does not really work.
    There are some workarounds:
    - <https://stackoverflow.com/questions/66389043/how-can-i-use-vite-env-variables-in-vite-config-js>
    - <https://github.com/vitejs/vite/issues/1930>
    
    Not sure if it is worth it ...

--- web-src/vite.config.js
+++ web-src/vite.config.js
@@ -4,5 +4,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: { alias: { '@': '/src' } },
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3689'
+      },
+      '/artwork': {
+        target: 'http://localhost:3689'
+      },
+      '/stream.mp3': {
+        target: 'http://localhost:3689'
+      }
+    }
+  }
 })
