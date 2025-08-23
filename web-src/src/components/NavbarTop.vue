b0eddba932aab3beb11e7e4abaaec94c1a5b3227
Christian Meffert <christian.meffert@googlemail.com>
2025-08-23 09:00:32 +0200

    [web] Show quick links in navbar on mobile

--- web-src/src/components/NavbarTop.vue
+++ web-src/src/components/NavbarTop.vue
@@ -5,7 +5,7 @@
         v-for="menu in menus.filter((menu) => menu.show && !menu.sub)"
         :key="menu.name"
         :to="{ name: menu.name }"
-        class="navbar-item is-hidden-mobile"
+        class="navbar-item"
       >
         <mdicon class="icon" :name="menu.icon" size="16" />
       </control-link>
